be70a4966ee3e909f8db30b6da05c72a9ecfaf02
azeezabass2005 <azeezabass2005@gmail.com>
2025-12-02 01:29:53 +0100

    fix: whatsapp initialization

--- src/app.ts
+++ src/app.ts
@@ -102,8 +102,8 @@ class App {
      * @private
      */
     private async connectWhatsapp(): Promise<void> {
-        await this.notificationService.disconnectWhatsApp();
-        // await this.notificationService.initializeWhatsApp();
+        // await this.notificationService.disconnectWhatsApp();
+        await this.notificationService.initializeWhatsApp();
     }
 
     /**
